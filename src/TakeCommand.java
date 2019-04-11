fdc3fa77cbf92afc950dcb977b387a22e18fe485
Ben Hertzberg <hertzberg.benjamin@gmail.com>
2019-04-10 20:55:28 -0700

    implement method signatures and add field to take command

--- src/TakeCommand.java
+++ src/TakeCommand.java
@@ -1,2 +1,14 @@
-public class TakeCommand {
+public class TakeCommand implements Command{
+    Level level;
+    String itemName;
+
+    @Override
+    public void init(String userString) {
+
+    }
+
+    @Override
+    public boolean execute() {
+        return false;
+    }
 }
